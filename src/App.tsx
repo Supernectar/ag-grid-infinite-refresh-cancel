43de34890700544e06ed060a64dab1e72c4ff394
Gonçalo Pataca <gonzalo.nascimento.pataca@gmail.com>
2025-01-31 16:53:46 +0000

    Adjusted grid height for code sandbox

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 import { useMemo, useCallback, useRef } from "react";
 import { AgGridReact, CustomCellRendererProps } from "ag-grid-react";
 import type { ColDef, IDatasource, IGetRowsParams } from "ag-grid-community";
@@ -12,7 +12,7 @@ import {
 ModuleRegistry.registerModules([
   PaginationModule,
   InfiniteRowModelModule,
-  ValidationModule
+  ValidationModule,
 ]);
 
 enum Status {
@@ -23,12 +23,12 @@ enum Status {
 interface Character {
   id: string;
   name: string;
-  status: Status
+  status: Status;
 }
 
-const defaultApiPageSize = 20  // default fixed rickandmortyapi page size
-const paginationPageSize = 12  // less than the rickandmortyapi default page size, this allows to populate multiple grid pages for each API call 
-const maxConcurrentDatasourceRequests = 2
+const defaultApiPageSize = 20; // default fixed rickandmortyapi page size
+const paginationPageSize = 12; // less than the rickandmortyapi default page size, this allows to populate multiple grid pages for each API call
+const maxConcurrentDatasourceRequests = 2;
 
 const getRandomStatus = (): Status => {
   const statuses = [Status.Alive, Status.Dead, Status.unknown];
@@ -37,59 +37,75 @@ const getRandomStatus = (): Status => {
 
 const fetchData = async (params: IGetRowsParams) => {
   const { startRow } = params;
-  const url = `https://rickandmortyapi.com/api/character?page=${Math.floor(startRow / defaultApiPageSize) + 1}`;
+  const url = `https://rickandmortyapi.com/api/character?page=${
+    Math.floor(startRow / defaultApiPageSize) + 1
+  }`;
 
   const response = await fetch(url);
 
   const jsonResponse = await response.json();
-  const characters: Character[] = jsonResponse.results.map((char: Character) => ({
-    ...char,
-    status: getRandomStatus(),
-  }));
+  const characters: Character[] = jsonResponse.results.map(
+    (char: Character) => ({
+      ...char,
+      status: getRandomStatus(),
+    })
+  );
 
-  return {totalDataCount: jsonResponse.info.count, pageData: characters};
+  return { totalDataCount: jsonResponse.info.count, pageData: characters };
 };
 
 export default function InfiniteGrid() {
   const gridRef = useRef<AgGridReact>(null);
-  const containerStyle = useMemo(() => ({ width: "100%", height: '600px' }), []);
+  const containerStyle = useMemo(
+    () => ({ width: "100%", height: "400px" }),
+    []
+  );
   const gridStyle = useMemo(() => ({ height: "100%", width: "100%" }), []);
-  const refreshButtonContainerStyle = useMemo(() => ({ display: 'flex', justifyContent: 'flex-end' }), []);
+  const refreshButtonContainerStyle = useMemo(
+    () => ({ display: "flex", justifyContent: "flex-end" }),
+    []
+  );
 
-  const columnDefs = useMemo<ColDef[]>(() => [
-    {
-      field: "id",
-      cellRenderer: (props: CustomCellRendererProps) => {
-        console.log(props)
-        if (props.value !== undefined) {
-          return props.value;
-        } else {
-          return (
-            <img src="https://www.ag-grid.com/example-assets/loading.gif" />
+  const columnDefs = useMemo<ColDef[]>(
+    () => [
+      {
+        field: "id",
+        cellRenderer: (props: CustomCellRendererProps) => {
+          console.log(props);
+          if (props.value !== undefined) {
+            return props.value;
+          } else {
+            return (
+              <img src="https://www.ag-grid.com/example-assets/loading.gif" />
+            );
+          }
+        },
+      },
+      { field: "name", flex: 1 },
+      { field: "status" },
+    ],
+    []
+  );
+
+  const dataSource = useMemo<IDatasource>(
+    () => ({
+      rowCount: undefined,
+      getRows: async (params) => {
+        try {
+          const { totalDataCount, pageData } = await fetchData(params);
+
+          params.successCallback(
+            pageData,
+            totalDataCount <= params.endRow ? totalDataCount : -1
           );
+        } catch (error) {
+          console.log("Fetch request failed", error);
+          params.failCallback();
         }
-      }
-    },
-    { field: "name", flex: 1 },
-    { field: "status" },
-  ], []);
-
-  const dataSource = useMemo<IDatasource>(() => ({
-    rowCount: undefined,
-    getRows: async (params) => {
-      try {
-        const {totalDataCount, pageData} = await fetchData(params);
-
-        params.successCallback(
-          pageData,
-          totalDataCount <= params.endRow ? totalDataCount : -1
-        );
-      } catch (error) {
-        console.log("Fetch request failed", error);
-        params.failCallback();
-      }
-    },
-  }), []);
+      },
+    }),
+    []
+  );
 
   const refreshData = useCallback(() => {
     gridRef.current?.api.purgeInfiniteCache();
